7f603e7f853daa6c23b78b3210ef07ea76a731a8
David Beck <dbeck@starschema.net>
2014-08-05 13:36:06 +0200

    integrate gtest library

--- proto.mk
+++ proto.mk
@@ -7,6 +7,14 @@ PROTO_OBJECTS := $(patsubst %.pb.cc,%.pb.o,$(PROTO_SOURCES))
 PROTO_DESCS := $(patsubst %.pb.cc,%.pb.desc,$(PROTO_SOURCES))
 PROTOBUF_LDFLAGS := $(shell pkg-config --libs protobuf)
 PROTOBUF_CFLAGS := $(shell pkg-config --cflags protobuf)
+GTEST_PATH := $(BUILD_ROOT)/src/gtest
+GTEST_CONFIG_STATUS := $(GTEST_PATH)/config.status
+# FIXME integrate libtool better ...
+GTEST_LIBDIR := $(GTEST_PATH)/lib/.libs/
+GTEST_INCLUDE := $(GTEST_PATH)/include
+GTEST_LIBS :=  $(GTEST_LIBDIR)/libgtest.a
+GTEST_LDFLAGS := $(GTEST_LIBS) -L$(GTEST_LIBDIR)
+GTEST_CFLAGS := -I$(GTEST_INCLUDE)
 
 # FIXME on Windows
 FIX_CXX_11_BUG =
@@ -16,13 +24,35 @@ FIX_CXX_11_BUG =  -Wl,--no-as-needed
 LINUX_LDFLAGS =  -pthread
 endif
 
-CXXFLAGS += -std=c++11 -fPIC $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_CFLAGS) 
-LDFLAGS += $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_LDFLAGS) 
+CXXFLAGS += -std=c++11 -fPIC $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_CFLAGS) $(GTEST_CFLAGS)
+LDFLAGS += $(FIX_CXX_11_BUG) $(LINUX_LDFLAGS) $(PROTOBUF_LDFLAGS) $(GTEST_LDFLAGS)
 
-all: proto-static-lib $(PROTO_DESCS)
+all: proto-static-lib gtest-pkg-build-all $(PROTO_DESCS)
 
 proto-static-lib: $(PROTO_OBJECTS)
 	ar rcs libproto.a $(PROTO_OBJECTS)
+
+gtest-pkg-build-all: gtest-pkg-configure gtest-pkg-lib
+
+gtest-pkg-configure: $(GTEST_CONFIG_STATUS)
+
+$(GTEST_CONFIG_STATUS):
+	@echo "doing configure in gtest in " $(GTEST_PATH) 
+	cd $(GTEST_PATH) ; ./configure
+	@echo "configure done in gtest"
+
+# NOTE: assumption: the libdir will be created during build
+gtest-pkg-lib: $(GTEST_LIBDIR)
+
+$(GTEST_LIBDIR):
+	@echo "building the gtest package"
+	@make -C $(GTEST_PATH)
+	@echo "building finished in gtest package"
+
+gtest-pkg-clean:
+	@echo "cleaning the gtest package"
+	@make -C $(GTEST_PATH) clean
+	@echo "cleaning finished in gtest package"
 
 %.pb.o: %.pb.cc %.pb.h
 
@@ -40,6 +70,6 @@ $(PROTO_PATH)/db_config.pb.cc: $(PROTO_PATH)/meta_data.pb.cc $(PROTO_PATH)/meta_
 
 $(PROTO_PATH)/svc_config.pb.cc: $(PROTO_PATH)/meta_data.pb.h
 
-clean:
+clean: gtest-pkg-clean
 	rm -f $(PROTO_OBEJCTS) *.a *.o *.pb.cc *.pb.h *.pb.desc
 
