e288a0d821dc7eae9e774f9cf512deb9641bca2b
David Beck <dbeck@starschema.net>
2014-08-07 10:15:01 +0200

    make sure we are on the master branch before pulling recursive submodules

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git submodule update --init --remote --recursive
-git pull --recurse-submodules
 git checkout master
+git pull --recurse-submodules 
 git remote add upstream https://github.com/starschema/virtdb-proto.git
 git fetch origin -v
 git fetch upstream -v
