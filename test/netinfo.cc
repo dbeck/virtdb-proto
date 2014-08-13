4e8aaa9a435987b681fc2cc3c0051fef9470ea4d
David Beck <dbeck@starschema.net>
2014-08-13 15:26:13 +0200

    cosmetic change

--- test/netinfo.cc
+++ test/netinfo.cc
@@ -8,12 +8,12 @@ int main()
 {
   std::string name(net::get_own_hostname());
   
-  std::cout << "My Hostname is: " << name << "\n\nIPS: ";
+  std::cout << "My Hostname is: " << name << "\n\n";
   
   net::string_vector ips{net::get_own_ips()};
   for( auto ip : ips )
   {
-    std::cout << " - " << ip << "\n";
+    std::cout << "IP: - " << ip << "\n";
   }
   
   return 0;
